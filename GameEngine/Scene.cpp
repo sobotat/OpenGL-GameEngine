50e9150904acb9cce55d17b97925ae56d8270751
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-09-28 11:40:42 +0200

    Created Scene

---
+++ GameEngine/Scene.cpp
@@ -0,0 +1,27 @@
+﻿#include "Scene.h"
+
+#include <GLFW/glfw3.h>
+
+#include "Aplication.h"
+
+void Scene::draw() {
+    // clear color and depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    for (Mesh* mesh : meshes) {
+        mesh->draw();
+    }
+
+    // update other events like input handling
+    glfwPollEvents();
+    // put the stuff we’ve been drawing onto the display
+    glfwSwapBuffers(Aplication::getInstance()->getWindow());
+}
+
+void Scene::addMesh(Mesh* mesh) {
+    meshes.push_back(mesh);
+}
+
+void Scene::destroyMesh(Mesh* mesh) {
+    meshes.erase(remove(meshes.begin(), meshes.end(), mesh), meshes.end());
+}
---
+++ GameEngine/Scene.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include <vector>
+
+#include "Meshes/Mesh.h"
+
+using namespace std;
+class Scene {
+protected:
+    vector<Mesh*> meshes;
+    
+public:
+    void draw();
+    void addMesh(Mesh* mesh);
+    void destroyMesh(Mesh* mesh);
+};
