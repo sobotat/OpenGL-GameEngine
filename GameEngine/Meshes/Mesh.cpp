1f0b44368bb0ca8a28684cb9346316834b720c72
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-09-28 12:44:04 +0200

    Fixed creating buffer object in mesh

--- GameEngine/Meshes/Mesh.cpp
+++ GameEngine/Meshes/Mesh.cpp
@@ -5,31 +5,26 @@
 #include <GL/glew.h>
 
 Mesh::Mesh(vector<float> meshPoints) {
-    points = move(meshPoints);
+    points = std::move(meshPoints);
         
     //vertex buffer object (VBO)
     glGenBuffers(1, &VBO); // generate the VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    //created array of points from vector
-    float* pointsArray = new float[points.size()];
-    copy(points.begin(), points.end(), pointsArray);
-    
-    glBufferData(GL_ARRAY_BUFFER, points.size(), pointsArray, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(float) * 3, &points.front(), GL_STATIC_DRAW);
 	
     //Vertex Array Object (VAO
     glGenVertexArrays(1, &VAO); //generate the VAO
     glBindVertexArray(VAO); //bind the VAO
     glEnableVertexAttribArray(0); //enable vertex attributes
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, points.size(), GL_FLOAT, GL_FALSE, 0, NULL);
+    glVertexAttribPointer(0, points.size() / 3, GL_FLOAT, GL_FALSE, 0, NULL);
 }
 
 void Mesh::tick() {}
 
 void Mesh::draw() {
     glBindVertexArray(VAO);        
-    glDrawArrays(GL_TRIANGLES, 0, points.size()); //mode,first,count
+    glDrawArrays(GL_TRIANGLES, 0, points.size() / 3); //mode,first,count
 }
 
 bool operator==(const Mesh& mesh1, const Mesh& mesh2) {
