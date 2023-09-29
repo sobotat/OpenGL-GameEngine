2b8aa534a48ba1277f06fd39c62ca009e35dc80e
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-09-29 20:47:59 +0200

    Removed some comments
    Took 10 minutes

--- GameEngine/Meshes/Mesh.cpp
+++ GameEngine/Meshes/Mesh.cpp
@@ -16,14 +16,14 @@ void Mesh::setPoints(vector<float> meshPoints) {
     points = std::move(meshPoints);
         
     //vertex buffer object (VBO)
-    glGenBuffers(1, &VBO); // generate the VBO
+    glGenBuffers(1, &VBO); 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(float), &points.front(), GL_STATIC_DRAW);
 	
-    //Vertex Array Object (VAO
-    glGenVertexArrays(1, &VAO); //generate the VAO
-    glBindVertexArray(VAO); //bind the VAO
-    glEnableVertexAttribArray(0); //enable vertex attributes
+    //Vertex Array Object (VAO)
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glEnableVertexAttribArray(0); 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 }
@@ -33,7 +33,7 @@ void Mesh::tick() {}
 void Mesh::draw() {
     shaderProgram->useProgram();
     glBindVertexArray(VAO);        
-    glDrawArrays(GL_TRIANGLES, 0, points.size()); //mode,first,count
+    glDrawArrays(GL_TRIANGLES, 0, points.size());
 }
 
 bool operator==(const Mesh& mesh1, const Mesh& mesh2) {
