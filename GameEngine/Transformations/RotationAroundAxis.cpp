d94590b0532ac10e7ff60ac0a93d55e6a001e133
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-10-28 18:52:29 +0200

    Fixed RotationAroundAxis from ContinuousRotationAroundAxis
    Took 2 minutes

--- GameEngine/Transformations/RotationAroundAxis.cpp
+++ GameEngine/Transformations/RotationAroundAxis.cpp
@@ -5,17 +5,27 @@
 #include "Rotation.h"
 #include "Scale.h"
 
-RotationAroundAxis::RotationAroundAxis(float angle, vec3 axis, mat4 yourMatrix, mat4 parentMatrix) {
+RotationAroundAxis::RotationAroundAxis(float angle, vec3 axis, mat4 actorMatrix, mat4 parentMatrix) {
     this->angle = radians(angle);
     this->axis = axis;
+    this->actorAndParentDistance = vec3(parentMatrix[3]) - vec3(actorMatrix[3]); 
+    this->actorMatrix = actorMatrix;
     this->parentMatrix = parentMatrix;
     
-    vec3 location = vec3(parentMatrix[3]) - vec3(yourMatrix[3]);
-    vec3 originalScaling = vec3(1) / vec3(length(yourMatrix[0]), length(yourMatrix[1]), length(yourMatrix[2]));
+    vec3 location = vec3(parentMatrix[3]) - vec3(actorMatrix[3]);
     
-    addTransform(make_shared<Scale>(originalScaling));
     addTransform(make_shared<Location>(location));
     addTransform(make_shared<Rotation>(angle, axis));
-    addTransform(make_shared<Location>(-location));
-    addTransform(make_shared<Scale>(1.0f / originalScaling));
+    addTransform(make_shared<Location>(-actorAndParentDistance));
+}
+
+mat4 RotationAroundAxis::transform() {
+    mat4 inverse = mat4(glm::inverse(mat3(actorMatrix)));
+    
+    mat4 matrix = mat4(1);
+    matrix *= inverse;
+    matrix *= TransformComposite::transform();
+    matrix *= mat4(mat3(actorMatrix));
+    
+    return matrix;
 }
--- GameEngine/Transformations/RotationAroundAxis.h
+++ GameEngine/Transformations/RotationAroundAxis.h
@@ -8,8 +8,11 @@ class RotationAroundAxis : public TransformComposite {
 protected:
     float angle;
     vec3 axis;
+    mat4 actorMatrix;
     mat4 parentMatrix;
-    
+    vec3 actorAndParentDistance;
+
 public:
     RotationAroundAxis(float angle, vec3 axis, mat4 yourMatrix, mat4 parentMatrix);
+    mat4 transform() override;
 };
